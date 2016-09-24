8596d14f0dc83eefd81ea4ae8463a1af19591180
Alex Martin <alex.martin@innn.es>
2016-09-24 21:19:49 +0200

    Deploy básico

---
+++ deploy.php
@@ -0,0 +1,37 @@
+<?php
+
+require __DIR__ . '/recipe/symfony3.php';
+
+// Set configurations
+set('repository', 'git@github.com:alexdw/symfony-pack.git');
+set('shared_files', ['app/config/parameters.yml']);
+
+
+// Configure servers
+server('prod', '54.93.44.118')
+    ->user('ubuntu')
+    ->pemFile('~/PEM/Master.pem')
+    ->env('deploy_path', '/var/www/html');
+
+$config = function () {
+    set("parameters", askConfirmation("¿Subir parameters local?"));
+};
+
+task("config", $config);
+task('parameters', function () {
+    if (get("parameters")) {
+        upload('app/config/parameters.yml', '{{release_path}}/app/config/parameters.yml');
+    }
+});
+
+task("gulp", function(){
+    runLocally("gulp");
+    upload('web/js', '{{release_path}}/web/js');
+    upload('web/css', '{{release_path}}/web/css');
+    upload('web/img', '{{release_path}}/web/img');
+    upload('web/fonts', '{{release_path}}/web/fonts');
+});
+
+before('deploy', 'config');
+after('deploy:shared', 'parameters');
+before('success', 'gulp');
